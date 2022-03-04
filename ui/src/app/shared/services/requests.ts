8aee051141b4c99501f38d90f1d1c38ea15b04f7
hr lee <hrlee@devstack.co.kr>
2022-03-04 11:50:01 +0900

    update: update header

--- ui/src/app/shared/services/requests.ts
+++ ui/src/app/shared/services/requests.ts
@@ -42,6 +42,7 @@ export default {
         superagent.post(keycloakUrl('/realms/argo/protocol/openid-connect/token'))
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .set("Access-Control-Allow-Origin", "*")
+        .set("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
         .send("grant_type=client_credentials")
         .send("client_id=argoworkflow")
         .send("client_secret=gE9avSRpz3GZsSWqxPcUWxI6wqBNPyaT")
