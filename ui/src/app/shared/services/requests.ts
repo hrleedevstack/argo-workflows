90184e073dd6f9a77aee41fbeec0fec857b16b08
hr lee <hrlee@devstack.co.kr>
2022-03-04 11:40:20 +0900

    update: add access-control-allow-origin

--- ui/src/app/shared/services/requests.ts
+++ ui/src/app/shared/services/requests.ts
@@ -41,6 +41,7 @@ export default {
     token(){
         superagent.post(keycloakUrl('/realms/argo/protocol/openid-connect/token'))
         .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set("Access-Control-Allow-Origin", "*")
         .send("grant_type=client_credentials")
         .send("client_id=argoworkflow")
         .send("client_secret=gE9avSRpz3GZsSWqxPcUWxI6wqBNPyaT")
